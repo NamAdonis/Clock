f7164a8251b7daa4e09bb3b52da0f1a860391172
NamAdonis <namadonis2003@gmail.com>
2022-08-25 22:35:43 +0700

    Finish 24 hour - timming application

--- TimingMachine/Form1.Designer.cs
+++ TimingMachine/Form1.Designer.cs
@@ -28,13 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.Reset = new System.Windows.Forms.Button();
             this.Start = new System.Windows.Forms.Button();
+            this.ClockTick = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Reset
             // 
-            this.Reset.Location = new System.Drawing.Point(183, 269);
+            this.Reset.Location = new System.Drawing.Point(197, 288);
             this.Reset.Name = "Reset";
             this.Reset.Size = new System.Drawing.Size(178, 59);
             this.Reset.TabIndex = 0;
@@ -44,7 +57,7 @@
             // 
             // Start
             // 
-            this.Start.Location = new System.Drawing.Point(417, 269);
+            this.Start.Location = new System.Drawing.Point(448, 288);
             this.Start.Name = "Start";
             this.Start.Size = new System.Drawing.Size(178, 59);
             this.Start.TabIndex = 1;
@@ -52,16 +65,130 @@
             this.Start.UseVisualStyleBackColor = true;
             this.Start.Click += new System.EventHandler(this.Start_Click);
             // 
+            // ClockTick
+            // 
+            this.ClockTick.Tick += new System.EventHandler(this.ClockTick_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(108, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(97, 143);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(251, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(139, 143);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(416, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(147, 143);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(589, 104);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(128, 143);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(162, 251);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Hours";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(355, 251);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Mins";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(525, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Secs";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(685, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "1/10";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(218, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 38);
+            this.label5.TabIndex = 10;
+            this.label5.Text = ":";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(384, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 38);
+            this.label6.TabIndex = 11;
+            this.label6.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(556, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 38);
+            this.label7.TabIndex = 12;
+            this.label7.Text = ".";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Start);
             this.Controls.Add(this.Reset);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +196,18 @@
 
         private System.Windows.Forms.Button Reset;
         private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Timer ClockTick;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- TimingMachine/Form1.cs
+++ TimingMachine/Form1.cs
@@ -16,15 +16,77 @@ namespace TimingMachine
         {
             InitializeComponent();
         }
-
+        private int countMili = 0;
+        private int countSec = 0;
+        private int countMin = 0;
+        private int countHour = 0;
         private void Reset_Click(object sender, EventArgs e)
         {
-
+            ClockTick.Stop();
+            Start.Enabled = true;
+            countMili = 0;
+            countSec = 0;
+            countHour = 0;
+            countMin = 0;
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            textBox4.Text = "0";
         }
 
         private void Start_Click(object sender, EventArgs e)
         {
+            ClockTick.Start();
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            textBox4.Text = "0";
+            Start.Enabled = false;
+        }
 
+        private void ClockTick_Tick(object sender, EventArgs e)
+        {
+            countMili++;
+            textBox4.Text = countMili.ToString();
+            if(countMili == 10)
+            {
+                countMili = 0;
+                textBox4.Text = "0";
+                countSec++;
+                if(countSec == 60)
+                {
+                    countSec = 0;
+                    textBox3.Text = "0";
+                    countMin++;
+                    if(countMin == 60)
+                    {
+                        countMin = 0;
+                        textBox2.Text = "0";
+                        countHour++;
+                        if(countHour == 24)
+                        {
+                            MessageBox.Show("Over the time of a day. Restart for continuing count");
+                        }
+                        else
+                        {
+                            textBox1.Text = countHour.ToString();
+                        }
+                    }
+                    else
+                    {
+                        textBox2.Text = countMin.ToString();
+                    }
+                    
+                }
+                else
+                {
+                    textBox3.Text = countSec.ToString();
+                }
+            }
+            else
+            {
+                textBox4.Text = countMili.ToString();
+            }
         }
     }
 }
