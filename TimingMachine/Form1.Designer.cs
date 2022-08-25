9c0c856095de3dcc1869c379685fe679eef5768e
NamAdonis <namadonis2003@gmail.com>
2022-08-25 21:57:48 +0700

    Add project files.

---
+++ TimingMachine/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TimingMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Reset = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(183, 269);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(178, 59);
+            this.Reset.TabIndex = 0;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(417, 269);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(178, 59);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Reset);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Start;
+    }
+}
+
